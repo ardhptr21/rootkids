0de891127c6f82a354e51dd187591f5496aee803
ardhptr21 <ardhiputrapradana21@gmail.com>
2026-01-03 11:54:24 +0700

    fix: fix wrong file script content

--- src/rootkids/__init__.py
+++ src/rootkids/__init__.py
@@ -1,17 +1,3 @@
-from typing import Literal
-
-HexMode = Literal["none", "\\x", "0x"]
-
-
-def string_to_hex(
-    value: str, *, mode: HexMode = "none", encoding: str = "utf-8", separator: str = ""
-):
-    raw = value.encode(encoding)
-    hex_bytes = [f"{b:02x}" for b in raw]
-
-    if mode == "none":
-        return separator.join(hex_bytes)
-    if mode == "\\x":
-        return separator.join(f"\\x{hb}" for hb in hex_bytes)
-    if mode == "0x":
-        return separator.join(f"0x{hb}" for hb in hex_bytes)
+from .core import *
+from .utils import *
+from .web import *
